39daad67d488af8fc65cbc67209753d3797fd3df
Petra Alice Mikova <petra.alice.mikova@gmail.com>
2018-10-04 13:45:40 +0200

    User entity javadoc and small changes

--- src/main/java/backend/entities/User.java
+++ src/main/java/backend/entities/User.java
@@ -3,30 +3,62 @@ package backend.entities;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Date;
+import java.util.Objects;
 
+/**
+ * This class represents a user in our system
+ * @author pmikova
+ */
 public class User {
 
     private String name;
     private String surname;
-    private int age;
     private int weight;
     private Gender gender;
     private Date birthdate;
 
 
-
+    /**
+     * Constructor to create object user.
+     * @param name name of the user
+     * @param surname surname of the user
+     * @param birthdate date of birth
+     * @param weight weight
+     * @param gender gender
+     */
     public User(String name, String surname, Date birthdate, int weight, Gender gender){
         this.name = name;
         this.surname = surname;
         this.weight = weight;
         this.gender = gender;
         this.birthdate = birthdate;
-
+// TODO: give each user a unique id
     }
 
-
-
+    /**
+     * Enum gender specifies user gender.
+     */
     public enum Gender{ MALE, FEMALE};
+
+
+
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return weight == user.weight &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(surname, user.surname) &&
+                gender == user.gender &&
+                Objects.equals(birthdate, user.birthdate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, weight, gender, birthdate);
+    }
 
     public String getName() {
         return name;
@@ -36,6 +68,28 @@ public class User {
         return surname;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    //TODO age must be generated from date of birth
     public int getAge() {
         throw new NotImplementedException();
     }
