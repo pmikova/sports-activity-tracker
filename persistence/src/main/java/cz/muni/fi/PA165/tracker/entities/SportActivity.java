878146aaf4a04a249c4d32496c08436af6bd3279
Dominik Bujna <domco@mail.muni.cz>
2018-10-25 12:10:44 +0200

    BurnedCaloriesDAOImpl, UserDAOImpl, ActivityRecordDAO first versions done, update queries missing

--- persistence/src/main/java/cz/muni/fi/PA165/tracker/entities/SportActivity.java
+++ persistence/src/main/java/cz/muni/fi/PA165/tracker/entities/SportActivity.java
@@ -1,12 +1,19 @@
 package cz.muni.fi.PA165.tracker.entities;
 
 
-import java.util.Vector;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 /**
  * This class represents a sports activity.
  * @author Dominik-Bujna
  */
+
+@Entity
+@Table(name = "Users")
 public class SportActivity {
 
 
@@ -16,45 +23,37 @@ public class SportActivity {
     some of them should contain distance information, which can be specified
     some of the activities have different intensity levels or style of performing, which is expressed by the categories
 
-
      */
+    @NotNull
+    @Column(length = 30, nullable = false)
     private String activityName;
+
+    @NotNull
     private boolean hasDistance;
-    private boolean hasCategories;
-    private Vector<String> categories;
 
-    public SportActivity(String activityName, boolean hasDistance, boolean hasCategories, Vector<String> categories) {
-        this.activityName = activityName;
-        this.hasDistance = hasDistance;
-        this.hasCategories = hasCategories;
-        this.categories = categories;
-    }
-
-    public SportActivity(String activityName, boolean hasDistance) {
-        this.activityName = activityName;
-        this.hasDistance = hasDistance;
-        this.hasCategories = false;
-    }
-
-
+    @NotNull
     public boolean hasDistance() {
         return hasDistance;
     }
 
-    public boolean hasCategories() {
-        return hasCategories;
-    }
-
-    public Vector<String> getCategories() {
-        return categories;
-    }
-
+    @NotNull
     public String getActivityName() {
         return activityName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        SportActivity toCompare = (SportActivity) o;
+        return this.hasDistance == toCompare.hasDistance() &&
+               this.activityName == toCompare.getActivityName();
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.activityName, this.hasDistance);
+    }
 
 
 
