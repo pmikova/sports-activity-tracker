64acd1ccacc52b6c98d59bddd872a726bfdc0e92
HonzaOstrava <422539@mail.muni.cz>
2018-10-26 19:42:03 +0200

    Added implementation for BurnedCalories and SportActivityDAOImpl

--- persistence/src/main/java/cz/muni/fi/PA165/tracker/dao/SportActivityDAOImpl.java
+++ persistence/src/main/java/cz/muni/fi/PA165/tracker/dao/SportActivityDAOImpl.java
@@ -1,8 +1,58 @@
 package cz.muni.fi.PA165.tracker.dao;
 
+import cz.muni.fi.PA165.tracker.entities.SportActivity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+// this marks class as DAO
+@Repository
+// define as a transaction
+@Transactional
 /**
  * Implementation of SportActivityDAO interface.
- * @author TODO
+ * @author HonzaOstrava
  *  */
-public class SportActivityDAOImpl {
+public class SportActivityDAOImpl implements SportActivityDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void create(SportActivity activity) {
+        if (activity == null){
+            throw new IllegalArgumentException("Activity can not be null!");
+        }
+        entityManager.persist(activity);
+    }
+
+    @Override
+    public SportActivity getById(Long id) {
+        return entityManager.find(SportActivity.class, id);
+    }
+
+    @Override
+    public List<SportActivity> getAll() {
+        return entityManager.createQuery("SELECT a FROM SportActivity a", SportActivity.class).getResultList();
+    }
+
+    @Override
+    public void update(SportActivity activity) {
+        if (activity == null){
+            throw new IllegalArgumentException("Activity can not be null!");
+        }
+        entityManager.merge(activity);
+    }
+
+    @Override
+    public void delete(SportActivity activity) {
+        if (activity == null){
+            throw new IllegalArgumentException("Activity can not be null!");
+        }
+        // TODO change to use find when the SportActivity class have ID
+        entityManager.remove(entityManager.merge(activity));
+    }
 }
--- persistence/src/main/java/cz/muni/fi/PA165/tracker/entities/BurnedCalories.java
+++ persistence/src/main/java/cz/muni/fi/PA165/tracker/entities/BurnedCalories.java
@@ -1,8 +1,76 @@
 package cz.muni.fi.PA165.tracker.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
 /**
- * This class should represent the calories table.
- * @author TODO
+ * This class represent amount of burned calories for some recorded activity.
+ * @author HonzaOstrava
  */
+@Entity
+@Table(name = "BurnedCalories")
 public class BurnedCalories {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private User user;
+
+    @NotNull
+    private ActivityRecord activityRecord;
+
+    @NotNull
+    private int burnedCalories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public ActivityRecord getActivityRecord() {
+        return activityRecord;
+    }
+
+    public int getBurnedCalories() {
+        return burnedCalories;
+    }
+
+    public void setBurnedCalories(int burnedCalories) {
+        this.burnedCalories = burnedCalories;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setActivityRecord(ActivityRecord activityRecord) {
+        this.activityRecord = activityRecord;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BurnedCalories that = (BurnedCalories) o;
+        return burnedCalories == that.burnedCalories &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(user, that.user) &&
+                Objects.equals(activityRecord, that.activityRecord);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, activityRecord, burnedCalories);
+    }
 }
