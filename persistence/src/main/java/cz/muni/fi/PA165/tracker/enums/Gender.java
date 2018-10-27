30f5820b615ca4e82ea9eef142bfc38bcb6a8870
Petra Alice Mikova <petra.alice.mikova@gmail.com>
2018-10-27 13:59:32 +0200

    Javadoc

--- persistence/src/main/java/cz/muni/fi/PA165/tracker/enums/Gender.java
+++ persistence/src/main/java/cz/muni/fi/PA165/tracker/enums/Gender.java
@@ -2,5 +2,6 @@ package cz.muni.fi.PA165.tracker.enums;
 
 /**
  * Enum gender specifies user gender.
+ * @author pmikova 433345
  */
-public enum Gender{ MALE, FEMALE};
+public enum Gender{ MALE, FEMALE}
