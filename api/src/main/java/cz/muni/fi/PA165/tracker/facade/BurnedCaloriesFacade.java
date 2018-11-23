c7486ba3a955900db4d5ec2389f3d1a3b273e3a2
HonzaOstrava <422539@mail.muni.cz>
2018-11-23 19:27:20 +0100

    Added javadoc to BurnedCaloriesFacade

--- api/src/main/java/cz/muni/fi/PA165/tracker/facade/BurnedCaloriesFacade.java
+++ api/src/main/java/cz/muni/fi/PA165/tracker/facade/BurnedCaloriesFacade.java
@@ -13,17 +13,49 @@ import java.util.List;
  */
 public interface BurnedCaloriesFacade {
 
+    /**
+     * Creates burnedCalories.
+     * @param burnedCalories burnedCalories to create
+     * @return id of created user
+     */
     Long create(BurnedCaloriesCreateDTO burnedCalories);
 
-    void delete(BurnedCaloriesDTO burnedCalories);
-
+    /**
+     * Update burnedCalories.
+     * @param burnedCalories burnedCalories to update
+     */
     void update(BurnedCaloriesDTO burnedCalories);
 
+    /**
+     * Delete burnedCalories.
+     * @param burnedCalories burnedCalories to delete
+     */
+    void delete(BurnedCaloriesDTO burnedCalories);
+
+    /**
+     * Get all burnedCalories.
+     * @return List of burnedCalories
+     */
     List<BurnedCaloriesDTO> getAll();
 
+    /**
+     * Get burnedCalories by id.
+     * @param id id to find by
+     * @return wanted burnedCalories
+     */
     BurnedCaloriesDTO getById(Long id);
 
+    /**
+     * Get burnedCalories by user.
+     * @param user user to find by
+     * @return List of burnedCalories
+     */
     List<BurnedCaloriesDTO> getByUser(UserDTO user);
 
+    /**
+     * Get burnedCalories by activityRecord.
+     * @param activityRecord activityRecord to find by
+     * @return List of burnedCalories
+     */
     List<BurnedCaloriesDTO> getByActivity(ActivityRecordDTO activityRecord);
 }
