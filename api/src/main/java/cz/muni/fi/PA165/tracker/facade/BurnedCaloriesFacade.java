174240dd3865707835822d4b4df5b29ee930609e
HonzaOstrava <422539@mail.muni.cz>
2018-11-23 18:21:25 +0100

    Filled BurnedCaloriesFacade. Implemented BurnedCaloriesDTO and BurnedCaloriesCreateDTO

--- api/src/main/java/cz/muni/fi/PA165/tracker/facade/BurnedCaloriesFacade.java
+++ api/src/main/java/cz/muni/fi/PA165/tracker/facade/BurnedCaloriesFacade.java
@@ -1,8 +1,29 @@
 package cz.muni.fi.PA165.tracker.facade;
 
+import cz.muni.fi.PA165.tracker.dto.ActivityRecordDTO;
+import cz.muni.fi.PA165.tracker.dto.BurnedCaloriesCreateDTO;
+import cz.muni.fi.PA165.tracker.dto.BurnedCaloriesDTO;
+import cz.muni.fi.PA165.tracker.dto.UserDTO;
+
+import java.util.List;
+
 /**
  * Facade interface for Burned Calories.
- * @author TODO
+ * @author HonzaOstrava
  */
 public interface BurnedCaloriesFacade {
+
+    Long create(BurnedCaloriesCreateDTO burnedCalories);
+
+    void delete(BurnedCaloriesDTO burnedCalories);
+
+    void update(BurnedCaloriesDTO burnedCalories);
+
+    List<BurnedCaloriesDTO> getAll();
+
+    BurnedCaloriesDTO getById(Long id);
+
+    List<BurnedCaloriesDTO> getByUser(UserDTO user);
+
+    List<BurnedCaloriesDTO> getByActivity(ActivityRecordDTO activityRecord);
 }
