d8c01a072ce45d3f7dd227baa3018095a8a0cd0f
Unknown <domco@mail.muni.cz>
2018-12-14 20:33:24 +0100

    Progress on MvcConfig

--- spring-mvc/src/main/java/cz/muni/fi/PA165/tracker/configuration/MvcConfiguration.java
+++ spring-mvc/src/main/java/cz/muni/fi/PA165/tracker/configuration/MvcConfiguration.java
@@ -2,36 +2,82 @@ package cz.muni.fi.PA165.tracker.configuration;
 
 //import cz.muni.fi.PA165.sampledata;
 
-import cz.muni.fi.PA165.tracker.controllers.ActivityRecordController;
-import cz.muni.fi.PA165.tracker.controllers.SportActivityController;
-import cz.muni.fi.PA165.tracker.controllers.UserController;
+import cz.muni.fi.PA165.sampledata.SampleDataConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.validation.Validator;
+
+
+
 
 
 @EnableWebMvc
 @Configuration
-//@Import({SampleDataConfiguration.class})
-@ComponentScan(basePackageClasses = {ActivityRecordController.class, SportActivityController.class, UserController.class})
-public class MvcConfiguration // implements WebMvcConfigurer
-{
+@Import({SampleDataConfiguration.class})
+@ComponentScan(basePackages = "cz.muni.fi.PA165.tracker.controllers")
+public class MvcConfiguration  implements WebMvcConfigurer {
+
+    private final static Logger log = LoggerFactory.getLogger(MvcConfiguration.class);
+
+    private static final String TEXTS = "Texts";
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        log.debug("mapping URL / to home view");
+        registry.addViewController("/").setViewName("home");
+    }
 
 
+    /**
+     * Enables default Tomcat servlet that serves static files.
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        log.debug("enabling default servlet for static files");
+        configurer.enable();
+    }
 
-//
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
-//
-//    }
-//
-//    @Override
-//    public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
-//
-//    }
-//
-//    }
+    /**
+     * Provides mapping from view names to JSP pages in WEB-INF/jsp directory.
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        log.debug("registering JSP in /WEB-INF/jsp/ as views");
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    /**
+     * Provides localized messages.
+     */
+    @Bean
+    public MessageSource messageSource() {
+        log.debug("registering ResourceBundle 'Texts' for messages");
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(TEXTS);
+        return messageSource;
+    }
+
+    /**
+     * Provides JSR-303 Validator.
+     */
+    @Bean
+    public Validator validator() {
+        log.debug("registering JSR-303 validator");
+        return new LocalValidatorFactoryBean();
+    }
 }
